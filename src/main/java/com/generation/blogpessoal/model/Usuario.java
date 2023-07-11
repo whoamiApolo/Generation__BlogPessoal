b143c09da4c33fddd86bd37f33467394199e0626
Apolinario <whoami.apolo@gmail.com>
2023-07-11 11:39:23 -0300

    Deploy do Projeto Blog Pessoal

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -28,6 +29,7 @@ public class Usuario {
 	@NotNull(message = "O atríbuto Nome é obrigatório!")
 	private String nome;
 
+	@Schema(example = "email@email.com.br")
 	@NotNull(message = "O atríbuto Usuário é obrigatório!")
 	@Email(message = "O atríbuto Usuário é obrigatório!")
 	private String usuario;
